efbb8d9183a4eb8712f17d163f61969a99b949ba
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-05-26 18:23:25 +0300

    after mentor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 import { fetchPhotosByQuery, PER_PAGE } from "./js/pixabay-api";
 import { createMarkupItem } from "./js/render-functions";
 
@@ -6,7 +7,6 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
@@ -18,21 +18,21 @@ let query = '';
 let photosCurrentPage = 1;
 let totalPages = 0; 
 
- const lightbox = new SimpleLightbox('ul.list a', {
+const lightbox = new SimpleLightbox('ul.list a', {
   captionsData: 'alt', 
   captionDelay: 250 ,
   overlayOpacity: 0.8,
 })
 
-const onSearchFormSubmit = async event =>{
+const onSearchFormSubmit = async event => {
     event.preventDefault();
     galleryEl.innerHTML = '';
     photosCurrentPage = 1;
 
     loadMoreBtnEl.classList.add('is-hidden'); 
 
-    const form= event.currentTarget; 
-     query = form.elements.searchKeyword.value.trim();
+    const form = event.currentTarget; 
+    query = form.elements.searchKeyword.value.trim();
     
     if (query === '') {
       iziToast.error({
@@ -42,10 +42,10 @@ const onSearchFormSubmit = async event =>{
         color: 'red',
       });
       form.reset();
-        return;
-      }
+      return;
+    }
 
-  try {
+    try {
       searchFormSubmitBtnEl.classList.remove('is-disabled');
       loaderEl.classList.remove('is-hidden');
 
@@ -61,87 +61,90 @@ const onSearchFormSubmit = async event =>{
         });
         form.reset();
         loaderEl.classList.add('is-hidden');
-      return;
+        return;
       }
 
       galleryEl.insertAdjacentHTML('beforeend', createMarkupItem(hits));
-      lightbox.refresh()
+      lightbox.refresh();
       loaderEl.classList.add('is-hidden');
       searchFormSubmitBtnEl.classList.add('is-disabled');
 
       totalPages = Math.ceil(totalHits / PER_PAGE);
       if (totalPages > 1) {
-        // Show laod more btn
+        // Show load more btn
         loadMoreBtnEl.classList.remove('is-hidden');
       }
- } catch (error){
-   searchFormSubmitBtnEl.classList.add('is-disabled')
-   loaderEl.classList.add('is-hidden');
-   iziToast.error({
-     message: 'Search params is not valid!',
-     position: 'topRight',
-    });
-    form.reset();
-    console.log("🚀 ~ onSearchFormSubmit ~ error:", error)
-    return;
-  };
-  
-  form.reset();
-};
-    searchFormEl.addEventListener('submit', onSearchFormSubmit);
-    const smoothScrollOnLoadMore = () => {
-      const lastPhoto = document.querySelector('.gallery__item:last-child');
-      const photosHeight = lastPhoto.getBoundingClientRect().height;
-      // console.log(" photosHeight:", photosHeight)
-      const twoPhotosHeight = photosHeight * 2;
-      // console.log("twoPhotosHeight:", twoPhotosHeight)
-      window.scrollBy({
-        top: twoPhotosHeight,
-        left: 0,
-        behavior: 'smooth',
-      });
-    };
-    const onLoadMorePress = async event =>{
-      try {
-        loadMoreBtnEl.classList.add('is-hidden');
 
-        loaderEl.classList.remove('is-hidden');
-    
-        photosCurrentPage += 1;
-    
-        const { hits, totalHits } = await fetchPhotosByQuery(query, photosCurrentPage);
-    
-        galleryEl.insertAdjacentHTML('beforeend', createMarkupItem(hits));
-    
-        lightbox.refresh();
-    
-        smoothScrollOnLoadMore();
-  
-        loaderEl.classList.add('is-hidden');
-    
-        totalPages = Math.ceil(totalHits / PER_PAGE);
-        if (photosCurrentPage < totalPages) {
-
-          loadMoreBtnEl.classList.remove('is-hidden');
-        } else {
-          loadMoreBtnEl.removeEventListener('click', onLoadMorePress);
-          iziToast.info({
-            message: "We're sorry, but you've reached the end of search results.",
-            position: 'topRight',
-          });
-          return;
-        }
-      } catch (error) {
-        searchFormSubmitBtnEl.classList.remove('is-disabled');
-  
-        loaderEl.classList.add('is-hidden');
-    
-        iziToast.error({
-          message: 'Search params is not valid!',
+      if (totalPages < 15) {
+        iziToast.info({
+          message: "We're sorry, but you've reached the end of search results",
           position: 'topRight',
+          timeout: 2000,
         });
-        form.reset();
-        return;
       }
+    } catch (error) {
+      searchFormSubmitBtnEl.classList.add('is-disabled');
+      loaderEl.classList.add('is-hidden');
+      iziToast.error({
+        message: 'Search params is not valid!',
+        position: 'topRight',
+      });
+      form.reset();
+      console.log("🚀 ~ onSearchFormSubmit ~ error:", error);
+      return;
+    };
+  
+    form.reset();
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
+
+const smoothScrollOnLoadMore = () => {
+  const lastPhoto = document.querySelector('.gallery__item:last-child');
+  const photosHeight = lastPhoto.getBoundingClientRect().height;
+  const twoPhotosHeight = photosHeight * 2;
+  window.scrollBy({
+    top: twoPhotosHeight,
+    left: 0,
+    behavior: 'smooth',
+  });
+};
+
+const onLoadMorePress = async event => {
+  try {
+    loadMoreBtnEl.classList.add('is-hidden');
+    loaderEl.classList.remove('is-hidden');
+
+    photosCurrentPage += 1;
+
+    const { hits, totalHits } = await fetchPhotosByQuery(query, photosCurrentPage);
+
+    galleryEl.insertAdjacentHTML('beforeend', createMarkupItem(hits));
+    lightbox.refresh();
+    smoothScrollOnLoadMore();
+
+    loaderEl.classList.add('is-hidden');
+
+    totalPages = Math.ceil(totalHits / PER_PAGE);
+    if (photosCurrentPage < totalPages) {
+      loadMoreBtnEl.classList.remove('is-hidden');
+    } else {
+      loadMoreBtnEl.removeEventListener('click', onLoadMorePress);
+      // iziToast.info({
+      //   message: "We're sorry, but you've reached the end of search results.",
+      //   position: 'topRight',
+      // });
+      return;
     }
-    loadMoreBtnEl.addEventListener('click', onLoadMorePress);
+  } catch (error) {
+    searchFormSubmitBtnEl.classList.remove('is-disabled');
+    loaderEl.classList.add('is-hidden');
+    iziToast.error({
+      message: 'Search params is not valid!',
+      position: 'topRight',
+    });
+    return;
+  }
+}
+
+loadMoreBtnEl.addEventListener('click', onLoadMorePress);
